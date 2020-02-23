a084fcdd1ee05cfef37d70561fd6629b435b3725
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-02-24 01:48:13 +0530

    print() and show() method is added

--- src/definition/ContactList.java
+++ src/definition/ContactList.java
@@ -6,7 +6,7 @@ public class ContactList<E> implements ContactAdt<E> {
         private Node<E> head = null;
         private int size = 0;
 
-        private Node<E> getNode(int index) {
+        public Node<E> getNode(int index) {
                 Node<E> response = head;
                 for (int i = 0; i < index; i++) {
                         response = response.getNext();
@@ -59,73 +59,164 @@ public class ContactList<E> implements ContactAdt<E> {
 
         // search contact
         @Override
-        public void searchContact(Person person) {
+        public int search(String item) {
+                Node check = head;
+                int c = 0;
+                while (check != null) {
+                        if (check.data.getFirstName().equals(item)) {
+                                c++;
+                        } else if (check == null) {
+                                int d = 0;
 
-                for (int i = 0; i < size; i++) {
-                        if (person.getFirstName().equals(this.getNode(i).getData().getFirstName())) {
-                                Node<E> temp = this.getNode(i);
-                                System.out.println(temp.getData());
-                        } else {
-                                System.out.println("NO RESULTS FOUND!");
                         }
+                        check = check.next;
+
+                }
+                return c;
+        }
+
+        public void printSearchitem(String name) {
+                Node check = head;
+                while (check != null) {
+                        System.out.println("........*.......*.......*.......*");
+                        if (check.data.getFirstName().equals(name)) {
+                                System.out.print("FirstName :" + check.data.getFirstName() + "\n");
+                                System.out.println("LastName :" + check.data.getLastName());
+                                if (check.data.getContactNo().size() > 1) {
+                                        System.out.println("Contacts :" + check.data.getContactNo());
+
+                                } else if (check.data.getContactNo().size() == 1) {
+                                        System.out.println("Contact :" + check.data.getContactNo());
+                                }
+                                System.out.println("email iD :" + check.data.getEmailAddress());
+                                System.out.println("........*.......*.......*.......*");
+
+                        }
+                        check = check.next;
                 }
         }
 
 
         //delete Method
-        private Person removeFirst() {
+        private Person removeAfter(Node node) {
                 Person response = null;
-
-                Node<E> temp = head;
-                if (head != null) {
-                        head = head.getNext();
-                        response = temp.getData();
+                Node temp = node.getNext();
+                if (temp != null) {
+                        node.next = temp.getNext();
                         size--;
+
+                        response = temp.getData();
                 }
                 return response;
         }
 
-        private Person removeAfter(Node<E> node) {
+        private Person removeFirst() {
                 Person response = null;
-                if (node.next != null) {
-                        node.next = node.getNext().getNext();
+                Node temp = head;
+                if (head != null) {
+                        head = head.getNext();
+                }
+                if (temp != null) {
                         size--;
-                        response = node.getNext().getData();
+                        response = temp.getData();
                 }
                 return response;
         }
 
         @Override
-        public Person deleteContact(int index) {
+        public boolean deleteContact(int index) {
                 Person response = null;
+                boolean re = false;
                 if (index < 0 || index > size) {
                         throw new IndexOutOfBoundsException(Integer.toString(index));
                 } else if (index == 0) {
                         response = removeFirst();
+
+                        re = true;
                 } else {
-                        response = removeAfter(getNode(index - 1));
+                        Node pr = getNode(index - 1);
+                        response = removeAfter(pr);
+                        re = true;
                 }
-                return response;
+                return re;
+        }
+
+        public void print() {
+                Node reference = head;
+                System.out.println("........*.......*.......*.......*");
+                while (reference != null) {
+                        System.out.print("FirstName :" + reference.data.getFirstName() + "\n");
+                        System.out.println("LastName :" + reference.data.getLastName());
+                        if (reference.data.getContactNo().size() > 1) {
+                                System.out.println("Contacts :" + reference.data.getContactNo());
+
+                        } else if (reference.data.getContactNo().size() == 1) {
+                                System.out.println("Contact :" + reference.data.getContactNo());
+                        }
+                        System.out.println("email iD :" + reference.data.getEmailAddress());
+                        System.out.println("........*.......*.......*.......*");
+                        System.out.println("........*.......*.......*.......*");
+                        reference = reference.next;
+                }
+        }
+
+        public void show() {
+                Node temp = head;
+                for (int i = 0; i < size; i++) {
+                        if (temp != null) {
+                                String name = temp.data.getFirstName() + " " + temp.data.getLastName();
+                                System.out.println(i + 1 + " . " + name);
+                        } else {
+                                System.out.println(" No contact found");
+                        }
+                        temp = temp.next;
+                }
+        }
+
+        public ContactList<Person> sort(ContactList<Person> lis) {
+                Node check = head;
+                Node index = null;
+                int status;
+                if (head == null) {
+                        return lis;
+                } else {
+                        while (check != null) {
+                                index = check.next;
+                                while (index != null) {
+                                        status = check.data.getFirstName().compareToIgnoreCase(index.data.getFirstName());
+                                        if (status > 0) {
+                                                Person temp = check.data;
+                                                check.data = index.data;
+                                                index.data = temp;
+                                                index = index.next;
+                                        } else {
+                                                index = index.next;
+                                        }
+                                }
+                                check = check.next;
+                        }
+                }
+                return lis;
         }
 
 
         //Node class
 
-        private static class Node<E> {
+        static class Node<E> {
 
                 private Person data;
                 private Node<E> next;
 
-                private Node(Person data, Node<E> next) {
+                public Node(Person data, Node<E> next) {
                         this.data = data;
                         this.next = next;
                 }
 
-                private Person getData() {
+                public Person getData() {
                         return data;
                 }
 
-                private Node<E> getNext() {
+                public Node<E> getNext() {
                         return next;
                 }
         }
